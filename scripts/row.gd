f77250db81c96c74a3af0f46ba5f2196e20bfef3
ag-elk <agnese.kirsblate@valmiera.edu.lv>
2025-03-26 22:35:06 +0200

    Delete scripts directory

--- scripts/row.gd
+++
@@ -1,6 +0,0 @@
-extends HBoxContainer
-
-
-func _ready():
-	for block in get_children():
-		block.flip_sound.pitch_scale = block.base_pitch + (0.1 * block.get_index())
--- scripts/signal_bus.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-signal game_started
-signal virtual_key_pressed(char: String)
-signal virtual_enter_pressed
-signal virtual_back_pressed
-signal row_finished(c_list: Array, h_list: Array, w_list: Array)
